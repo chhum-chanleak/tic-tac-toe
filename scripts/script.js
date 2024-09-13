202c49bc7a6ec6d1253811ae1b208944df27f5bc
Chanleak Chhum <chhum.chanleak1994@gmail.com>
2024-09-13 15:19:23 +0700

    refactor: Update hasWon(), so it no longer increases players' score

--- scripts/script.js
+++ scripts/script.js
@@ -269,14 +269,13 @@ const game = (() => {
     }
     return false;
   };
-
+  
   // Check an array and return true when all array's item has the same values.
   const hasWon = (contents) => { 
     for (let i = 0; i < contents.length; i += 1) {
       if (contents[i] === contents[i + 1] && contents[i + 1] === contents[i + 2]) {
         if (contents[i][0] === 'X') {
             winner = 'Winner is player1.';
-            player.increaseScore(`player1`);
 
             // Remove event(stop the game) from each box of the board when the game is won.
             game.removeEvent(board.getBoxesFromBoard());
@@ -284,7 +283,6 @@ const game = (() => {
             return true;             
           } else if (contents[i][0] === 'O') {
             winner = 'Winner is player2.';
-            player.increaseScore(`player2`);
 
             // Remove event(stop the game) from each box of the board when the game is won.
             game.removeEvent(board.getBoxesFromBoard());
